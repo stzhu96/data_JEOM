55f3e18f3472c09d91746f3b6cdc32fa81b0851f
stzhu96 <96108948+stzhu96@users.noreply.github.com>
2025-11-03 09:30:28 +0800

    Update 3中介分析示例代码.R

--- 3中介分析示例代码.R
+++ 3中介分析示例代码.R
@@ -10,7 +10,7 @@ model2 <- plm(con ~ bc + bmi + zrea + sex + eduac + age + marac + retir + hibpe 
               index = c("ID2", "time"), 
               data = df2, 
               model = "within")  
-# 计算BC对BMI的影响，BMI对认知的影响
+# 计算BC对BMI的影响，BMI对认知的影响。
 # 从 `model1` 模型中提取 BC 对 BMI 的影响系数
 a <- coef(model1)["bc"] 
 # 从 `model2` 模型中提取 BMI 对认知（con）的影响系数
@@ -19,14 +19,14 @@ b <- coef(model2)["bmi"]
 # 计算BC对因变量con的直接影响）
 # 从 `model2` 模型中提取 BC 对认知（con）的直接影响系数
 c <- coef(model2)["bc"]  
-# 计算中介效应 = a * b, 中介效应等于路径a和路径b的乘积。
+# 计算中介效应 = a * b, 中介效应等于BMI对认知的影响系数和BMI对认知的乘积。
 mediation_effect <- a * b  
 
 # 计算路径a的标准误差，vcov函数返回模型系数的协方差矩阵。
 se_a <- sqrt(vcov(model1)["bc", "bc"])  
 # 计算路径b的标准误差，vcov函数返回模型系数的协方差矩阵。
 se_b <- sqrt(vcov(model2)["bmi", "bmi"])  
-# 计算中介效应的标准误差（假设BC对BMI的影响，BMI对认知的影响独立）
+# 计算中介效应的标准误差（假设BC对BMI的影响，BMI对认知的影响独立）。
 se_mediation <- sqrt((b * se_a)^2 + (a * se_b)^2)  
 # 计算t值和p值，用于检验中介效应是否显著。
 # 中介效应的t值
@@ -34,7 +34,7 @@ t_value <- mediation_effect / se_mediation
 # p值，根据t值和自由度计算
 p_value <- 2 * (1 - pt(abs(t_value), df = nrow(df2) /3- 2))  
 
-# 计算总效应, 总效应是BC 对认知（con）的直接影响系数和中介效应a * b的和。
+# 计算总效应, 总效应是BC对认知的直接影响系数和中介效应的和。
 total_effect <- c + a * b  
 # 计算总效应的标准误差
 # 总效应的标准误差
@@ -43,3 +43,4 @@ se_total <- sqrt((se_a^2 * b^2) + (se_b^2 * a^2))
 t_value_total <- total_effect / se_total 
 # 计算总效应的p值
 p_value_total <- 2 * (1 - pt(abs(t_value_total), df = nrow(df2) /3- 2))
+
